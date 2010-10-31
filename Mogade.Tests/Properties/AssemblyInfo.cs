9c6b18473f84169f7eb5e300b2c5db2a3757aeba
Karl Seguin <karl@openmymind.net>
2010-10-31 06:19:40 -0400

    initial commit

---
+++ Mogade.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mogade.NET Tests")]
+[assembly: AssemblyDescription("Tests for Mogade.com's Official .NET Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mogade")]
+[assembly: AssemblyProduct("Mogade.NET")]
+[assembly: AssemblyCopyright("Copyright © Mogade 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c6f54d52-3824-431a-af9d-3e531a862d92")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Mogade/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mogade.NET")]
+[assembly: AssemblyDescription("Mogade.com's Official .NET Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mogade")]
+[assembly: AssemblyProduct("Mogade.NET")]
+[assembly: AssemblyCopyright("Copyright © Mogade 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("dbc3f308-ac79-4279-916f-2ba1b8b42e08")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
